94a9d73f521dbbe136aa2b84ba19a2a3da74c4db
Ruben <chickenzrage@gmail.com>
2016-08-20 20:40:20 +0100

    Adds forum ranks as an enum to the user table

--- users.sql
+++ users.sql
@@ -1,3 +1,8 @@
+-- Create enum for user ranks
+-- Remember to cast type when comparing enums in query
+-- TODO Use string or use int? int might be better when ordering ranks
+CREATE TYPE rank AS ENUM ('normie', 'moderator', 'admin');
+
 CREATE TABLE users(
 	id BIGSERIAL primary key,
 	f_name varchar(20) not null,
@@ -5,5 +10,5 @@ CREATE TABLE users(
 	username varchar(30) not null,
 	password_hash varchar(256) not null,
 	email varchar(50) not null,
-	group_id int references groups(id)
+	user_rank rank not null
 );
