ba5fa2ba1c955535b88cd3f65ae8d3c2118a6f5d
Jake Torrance <jaket1234@hotmail.com>
2016-09-23 18:42:56 -0400

    Added stickies and edited timestamps

--- posts.sql
+++ posts.sql
@@ -2,6 +2,7 @@ CREATE TABLE posts (
 	id BIGSERIAL primary key,
 	content text not null,
 	timestamp timestamp without time zone default (now() at time zone 'utc') not null,
+	edited_timestamp timestamp without time zone default (now() at time zone'utc') not null,
 	thread_id int not null references threads(id),
 	user_id int not null references users(id)
 )
--- threads.sql
+++ threads.sql
@@ -3,6 +3,7 @@ CREATE TABLE threads (
 	title VARCHAR(256) not null,
 	timestamp timestamp without time zone default (now() at time zone 'utc') not null,
 	locked boolean DEFAULT false not null,
+	sticky boolean DEFAULT false not null,
 	subcategory_id int not null,
 	user_id int not null REFERENCES users(id),
 	FOREIGN KEY (subcategory_id) REFERENCES subcategories (id)
